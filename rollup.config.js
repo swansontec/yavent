5d30defa01b77927e29dc74b21eb1d91499c761b
William Swanson <swansontec@gmail.com>
2021-07-23 14:05:33 -0700

    Upgrade to rollup-plugin-mjs-entry

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import filesize from 'rollup-plugin-filesize'
 import flowEntry from 'rollup-plugin-flow-entry'
+import mjsEntry from 'rollup-plugin-mjs-entry'
 
 import packageJson from './package.json'
 
@@ -35,28 +36,6 @@ export default {
     babel(babelOpts),
     flowEntry({ types: 'src/index.flow.js' }),
     filesize(),
-    mjs()
+    mjsEntry()
   ]
-}
-
-/**
- * Tiny plugin to generate .mjs wrappers for each entry point.
- */
-function mjs() {
-  return {
-    name: 'rollup-plugin-mjs',
-    generateBundle(options, bundle) {
-      if (options.format !== 'cjs') return
-      for (const fileName of Object.keys(bundle)) {
-        const chunk = bundle[fileName]
-        if (chunk.type !== 'chunk' || !chunk.isEntry) continue
-        const names = chunk.exports.join(',\n  ')
-        this.emitFile({
-          type: 'asset',
-          fileName: fileName.replace(/\.js$/, '') + '.mjs',
-          source: `import cjs from './${fileName}';\n\nexport const {\n  ${names}\n} = cjs;\n`
-        })
-      }
-    }
-  }
 }
